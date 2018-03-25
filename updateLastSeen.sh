d8d840e3a47a13d39ddb88163e395d2f28d122b9
kurairaito <kurairaito@gmail.com>
2018-03-25 12:01:05 +0200

    restart hci0 if lescan fail

--- updateLastSeen.sh
+++ updateLastSeen.sh
@@ -2,19 +2,26 @@
 
 FILENAME="/volume1/homes/admin/domoticz/last_seen_tag"
 
-stdbuf -i0 -o0 -e0 /bin/hcitool lescan | while read -r line; do
-    TIMESTAMP=`date +%s`;
-    stringarray=($line)
-    if [[ ${stringarray[0]} == ??:??:??:??:??:?? ]]
-    then
-        if grep -Rq "${stringarray[0]}" $FILENAME
-        then
-            #found in file : update timestamp
-            sed -i "s/.* ${stringarray[0]}/$TIMESTAMP ${stringarray[0]}/" $FILENAME
-        else
-            # not found in file : append
-            echo $TIMESTAMP ${stringarray[0]} >> $FILENAME
-        fi
-    fi
+while true; do
 
+	stdbuf -i0 -o0 -e0 /bin/hcitool lescan | while read -r line; do
+		TIMESTAMP=`date +%s`;
+		stringarray=($line)
+		if [[ ${stringarray[0]} == ??:??:??:??:??:?? ]]
+		then
+			if grep -Rq "${stringarray[0]}" $FILENAME
+			then
+				#found in file : update timestamp
+			    sed -i "s/.* ${stringarray[0]}/$TIMESTAMP ${stringarray[0]}/" $FILENAME
+				# not found in file
+			else
+			    # not found in file : append
+				echo $TIMESTAMP ${stringarray[0]} >> $FILENAME
+			fi
+		fi
+
+	done
+	hciconfig hci0 down
+	hciconfig hci0 up
 done
+
